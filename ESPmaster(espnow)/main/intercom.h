197ed88d768eed2f839f6e1b0f475395f1508587
Cristiano <cristolsan.cts.droid@droidunb.com>
2026-07-10 00:14:06 -0300

    Remove o master

--- ESPmaster(espnow)/main/intercom.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void intercom_init(void);
-void intercom_publish(const char *topic, const char *data);
-void intercom_read(char *out);
--- ESPmaster(espnow)/main/intracom.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-void intracom_init(void);
-void intracom_send(const char *message, int slave_index);
-void intracom_read(char *out_msg);
