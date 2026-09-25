33a646dad17945fdee0a368be9f8e897d409b5c1
Cristiano <cristolsan.cts.droid@droidunb.com>
2026-09-25 12:49:01 -0300

    Removendo arquivos n

--- SensoresAndAtuadores/main/curtain.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <stdbool.h>
-#include <stdint.h>
-
-extern int16_t carga_termica;
-
-void curtain_init(void);
--- SensoresAndAtuadores/main/temperature.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <stdbool.h>
-#include <stdint.h>
-
-void DS18B20_init(void);
-bool DS18B20_read(float *out_celsius);
