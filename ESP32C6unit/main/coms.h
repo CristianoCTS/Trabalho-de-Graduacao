ca466907cd2c26f75c667359f666773ffdd65ce3
Cristiano <cristolsan.cts.droid@droidunb.com>
2026-09-25 10:52:25 -0300

    Removendo arquivos desnecessarios para esse branch

--- ESP32C6unit/main/coms.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-void coms_init(void);
-void intercom_send(const char *data);
-void intercom_read(char *out);
-void intracom_send(const char *message, int slave_index);
-void intracom_read(char *out_msg);
